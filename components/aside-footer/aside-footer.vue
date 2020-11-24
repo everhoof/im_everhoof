ac5afdefd49aefce57a03f45359b93f64123d995
intelrug <intelrug@gmail.com>
2020-11-25 00:19:24 +0400

    feat(aside-footer): add logout button

--- components/aside-footer/aside-footer.vue
+++ components/aside-footer/aside-footer.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- begin .aside-footer-->
   <div class="aside-footer">
-    <div class="aside-footer__profile">
+    <div class="aside-footer__profile" @click.stop="dropdown = !dropdown">
       <img v-if="avatar" :src="avatar" class="aside-footer__avatar" />
       <div
         v-else
@@ -13,20 +13,43 @@
       <div class="aside-footer__info">
         <h3 class="aside-footer__username">{{ username }}</h3>
       </div>
+      <transition>
+        <ul v-show="dropdown" ref="dropdown" class="aside-footer__dropdown">
+          <li class="aside-footer__dropdown-item">
+            <b-button width-full>Профиль</b-button>
+          </li>
+          <li class="aside-footer__dropdown-item">
+            <b-button width-full @click.stop="logout">Выйти</b-button>
+          </li>
+        </ul>
+      </transition>
     </div>
   </div>
   <!-- end .aside-footer-->
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'nuxt-property-decorator';
+import { Component, Vue, Ref } from 'nuxt-property-decorator';
 import { getUserColor } from '~/tools/util';
 import { AuthState } from '~/store/auth';
+import BButton from '~/components/button/button.vue';
 
 @Component({
   name: 'b-aside-footer',
+  components: { BButton },
 })
 export default class AsideFooter extends Vue {
+  @Ref('dropdown') dropdownEl!: HTMLUListElement;
+  dropdown: boolean = false;
+
+  mounted() {
+    document.addEventListener('click', (event) => {
+      if (!this.dropdownEl.contains(event.target)) {
+        this.dropdown = false;
+      }
+    });
+  }
+
   get user(): AuthState['user'] {
     return this.$accessor.auth.user;
   }
@@ -42,7 +65,22 @@ export default class AsideFooter extends Vue {
   get avatarColor() {
     return getUserColor(this.user?.id || 0);
   }
+
+  logout() {
+    this.$apolloHelpers.onLogout();
+    this.$accessor.auth.SET_LOGGED_IN(false);
+  }
 }
 </script>
 
+<style lang="stylus" scoped>
+.v-enter-active,
+.v-leave-active
+  transition all .2s
+
+.v-enter,
+.v-leave-to
+  margin 8px 0
+  opacity 0
+</style>
 <style lang="stylus" src="./aside-footer.styl" />
