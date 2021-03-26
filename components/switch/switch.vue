8587f1bf32890e74e03d60f56c376bc5f0043c11
intelrug <intelrug@gmail.com>
2021-03-26 17:08:05 +0300

    feat: create switch component

---
+++ components/switch/switch.vue
@@ -0,0 +1,33 @@
+<template>
+  <!-- begin .switch-->
+  <div
+    class="switch"
+    :class="{
+      switch_with_margin: margin,
+    }"
+  >
+    <input :id="id" v-model="checkedSynced" class="switch__input" type="checkbox" :disabled="disabled" />
+    <label :for="id" class="switch__label">
+      <slot />
+    </label>
+  </div>
+  <!-- end .switch-->
+</template>
+
+<style lang="stylus" src="./switch.styl" />
+
+<script lang="ts">
+import { Component, Prop, PropSync, Vue } from 'nuxt-property-decorator';
+
+@Component({
+  name: 'b-switch',
+})
+export default class Switch extends Vue {
+  @Prop({ type: String, required: true }) id!: string;
+  @Prop({ type: String }) label!: string;
+  @Prop({ type: Boolean, default: false }) disabled!: boolean;
+  @Prop({ type: Boolean }) margin!: boolean;
+  @PropSync('checked', { required: false, type: Boolean })
+  checkedSynced!: boolean;
+}
+</script>
