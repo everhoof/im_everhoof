26337c4793fca6afac2635cac2269919301bcfed
intelrug <intelrug@gmail.com>
2022-09-13 02:15:52 +0300

    ci: archive artifacts on deploy

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 export NVM_DIR=~/.nvm
 source ~/.nvm/nvm.sh
 
-find . -maxdepth 1 ! -name node_modules ! -name artifacts ! -name . ! -name .. -exec rm -rf {} \;
-mv artifacts/* .
-mv artifacts/.[^.]* .
-rm -r artifacts
+find . -maxdepth 1 ! -name node_modules ! -name artifacts.tgz ! -name . ! -name .. -exec rm -rf {} \;
+tar -xvf artifacts.tgz
+rm artifacts.tgz
 
 NODE_ENV=$NODE_ENV pm2 stop ecosystem.config.js
 nvm exec 14.18.1 npm i -g yarn
 nvm exec 14.18.1 yarn --frozen-lockfile --production
 NODE_ENV=$NODE_ENV pm2 start ecosystem.config.js
+pm2 save
